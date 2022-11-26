f8ee33ff61aeb060d36c9f0f3852e9efeb54808e
Alwar G <31626278+AlwarG@users.noreply.github.com>
2022-11-26 16:49:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const {
   MongoClient
 } = require('mongodb');
 let dbConnetion;
-let uri = 'mongodb+srv://alwargak:EE0KeZ5sVCUhdNRZ@cluster0.epiedne.mongodb.net/?retryWrites=true&w=majority';
+let uri = 'mongodb+srv://<username>:<pwd>@cluster0.epiedne.mongodb.net/?retryWrites=true&w=majority';
 module.exports = {
   connectTODB(cb) {
     MongoClient.connect(uri).then((client) => {
